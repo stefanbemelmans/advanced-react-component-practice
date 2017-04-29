1c10d8f463949245e10fc6e38996bab7d181312d
Jon Woo <jw56578@gmail.com>
2017-04-29 18:52:29 -0500

    remove

--- src/App.js
+++ src/App.js
@@ -73,18 +73,6 @@ function App(props) {
                         </ol>
                     </div>
                 </div>
-                
-
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="alert alert-info alert-dismissable">
-                            <button type="button" className="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                            <i className="fa fa-info-circle"></i>  <strong>Like SB Admin?</strong> Try out <a href="http://startbootstrap.com/template-overviews/sb-admin-2" className="alert-link">SB Admin 2</a> for additional features!
-                        </div>
-                    </div>
-                </div>
-                
-
                 <div className="row">
 
                    {/*   </Comments>   */}
